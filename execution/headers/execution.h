99d83180093112ee9d92afa9b02dc243ff4afd4d
MAR BEN <marouanebenbajja@gmail.com>
2022-06-21 16:51:07 +0100

    Adding execution files

---
+++ execution/headers/execution.h
@@ -0,0 +1,15 @@
+#ifndef EXEC_H
+# define EXEC_H
+
+//	***********		Includes	*********** :
+#include "../../minishell.h"
+
+//	***********		Functions	*********** :
+	//	->	ft_cd :
+	//	->	ft_echo :
+	//	->	ft_env :
+	//	->	ft_exit :
+	//	->	ft_export :
+	//	->	ft_pwd :
+	//	->	ft_unset :
+#endif
